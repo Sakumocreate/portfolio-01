0b0a1945fdf67efec708c7fdad930ebdc1dc210f
Sakumocreate <sakumocreate@gmail.com>
2025-04-25 10:50:32 +0900

    Create javamon.java

---
+++ pokimon.java
@@ -0,0 +1,7 @@
+class main{public static void Main (String[] args){
+String name = "太郎";        
+System.out.print("おぬしが"+name+"くんじゃな？");
+name="あまぞんがわ";    
+System.out.println (name+"この世界にはふしぎなふしぎな生き物がいるんじゃ");
+ }
+}
